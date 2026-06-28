b054a3fa08cbcc908c2518c26351b35e2643af44
Cedric <djouyaguengc@gmail.com>
2026-06-28 08:30:45 +0100

    changement de logo

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,9 @@ import './globals.css';
 export const metadata = {
   title: 'Djeson Pro Assistant',
   description: 'Assistant expert en pronostics sportifs',
+  icons: {
+    icon: '/dembele.jpeg',
+  },
 };
 
 export default function RootLayout({ children }) {
